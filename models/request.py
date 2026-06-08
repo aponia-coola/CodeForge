01a2227038cca179db6a3b46fe0431162403fda3
cola_estarhy <2653603072@qq.com>
2026-06-08 16:05:31 +0800

    框架

--- models/request.py
+++ models/request.py
@@ -1,17 +1,13 @@
-import os
 from openai import OpenAI
-
 
 def request(message,modelname,action,):
     client = OpenAI(
         api_key=os.environ.get('DEEPSEEK_API_KEY'),
         base_url="https://api.deepseek.com"
     )
-
     response = client.chat.completions.create(
         model=modelname,
         messages=[
-            {"role": "system", "content": action},
             {"role": "user", "content": message},
         ],
         stream=False,
@@ -19,3 +15,4 @@ def request(message,modelname,action,):
         extra_body={"thinking": {"type": "enabled"}}
     )
     return response.choices[0].message.content
+
